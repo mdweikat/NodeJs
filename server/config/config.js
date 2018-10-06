93908fc2225256b5aad5be2b2b210acb3f2d212a
Murad Dweikat <m.dweikat1994@gmail.com>
2018-10-06 15:02:59 +0300

    Add Creator field in Todo Table. Add Test Case for all /todos api.

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 
 if (env === 'test') {
-  console.log('test test');
-  process.env.PORT = 3001;
+  process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else {
   process.env.PORT = 3000;
